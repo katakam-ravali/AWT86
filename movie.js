3537feb34d216efb004fcbf1f09694d7a0ba8c9a
katakam-ravali <ravali.katakam30@gmail.com>
2022-03-24 12:00:09 +0530

    24-03-2022 PROGRAMS

--- movie.js
+++ movie.js
@@ -1,6 +1,6 @@
 async function display()
 {
-    const url='https://imdb-api.com/API/AdvancedSearch/k_y87yrb5g?title_type=feature&user_rating=9,10&genres=family&countries=in&languages=te';
+    const url='https://imdb-api.com/API/AdvancedSearch/k_y87yrb5g?title_type=feature&user_rating=8.5,10&genres=family&countries=in&languages=te';
     const response=await fetch(url);
     var data=await response.json();
     return Promise.resolve(data);
@@ -9,9 +9,10 @@ display().then(function(data)
 {
     console.log(data)
     let{results}=data;
+    table=  document.getElementById("table");
     results.forEach(element => {
-        let{title,imDbRating}=element;
-        console.log('Title is '+title+' and Rating is '+imDbRating);
+        let{title,imDbRating}=element;   
+      table.innerHTML+='<tr><td>'+title+'</td><td>'+imDbRating+'</td></tr>';
     })
 },
 function()
